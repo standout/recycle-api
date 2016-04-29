44e0cfe61825df43e1240b28f1246213f8b6d1f9
Kevin Sjöberg <kev.sjoberg@gmail.com>
2016-04-29 02:09:46 +0200

    Add seed data for recycle locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+csv = CSVImport.new(File.expand_path("../seeds/recycle_locations.csv", __FILE__))
+csv.import(:fti)
